58cda6d79566284ae6f5248246d33d378e22225f
Joseph Lisee <jlisee@gmail.com>
2014-11-22 15:40:46 -0500

    Formatting fixes and refactoring out core functionality into functions
    We want to be able to check whether a specified program is running without
    actually exiting the current program. So we need access to these methods.

--- singleton.py
+++ singleton.py
@@ -16,46 +16,80 @@ from multiprocessing import Process
 class SingleInstance:
 
     """
-    If you want to prevent your script from running in parallel just instantiate SingleInstance() class. If is there another instance already running it will exist the application with the message "Another instance is already running, quitting.", returning -1 error code.
+    If you want to prevent your script from running in parallel just instantiate
+    SingleInstance() class. If is there another instance already running it will
+    exit the application with the message "Another instance is already running,
+    quitting.", returning -1 error code.
 
     >>> import tendo
     ... me = SingleInstance()
 
-    This option is very useful if you have scripts executed by crontab at small amounts of time.
+    This option is very useful if you have scripts executed by crontab at small
+    amounts of time.
 
-    Remember that this works by creating a lock file with a filename based on the full path to the script file.
+    Remember that this works by creating a lock file with a filename based on
+    the full path to the script file.
     """
 
     def __init__(self, flavor_id=""):
         import sys
         self.initialized = False
-        basename = os.path.splitext(os.path.abspath(sys.argv[0]))[0].replace("/", "-").replace(":", "").replace("\\", "-") + '-%s' % flavor_id + '.lock'
-        # os.path.splitext(os.path.abspath(sys.modules['__main__'].__file__))[0].replace("/", "-").replace(":", "").replace("\\", "-") + '-%s' % flavor_id + '.lock'
-        self.lockfile = os.path.normpath(tempfile.gettempdir() + '/' + basename)
+        self.lockfile = self.lockfile_path(flavor_id)
 
         logger.debug("SingleInstance lockfile: " + self.lockfile)
+
+        self.fobj = self.acquire_lockfile(self.lockfile)
+
+        if self.fobj is None:
+            logger.error("Another instance is already running, quitting.")
+            sys.exit(-1)
+
+        self.initialized = True
+
+    @staticmethod
+    def lockfile_path(flavor_id=""):
+        """
+        Generates a lock file path based on the location of the executable,
+        and flavor_id.
+        """
+
+        program_dir, _ = os.path.splitext(os.path.abspath(sys.argv[0]))
+        basename = program_dir. \
+            replace("/", "-"). \
+            replace(":", ""). \
+            replace("\\", "-") + '-%s' % flavor_id + '.lock'
+
+        return os.path.normpath(tempfile.gettempdir() + '/' + basename)
+
+    @staticmethod
+    def acquire_lockfile(lockpath):
+        """
+        If successful returns the lock file object, otherwise None.
+        """
+        fobj = None
+
         if sys.platform == 'win32':
             try:
-                # file already exists, we try to remove (in case previous execution was interrupted)
-                if os.path.exists(self.lockfile):
-                    os.unlink(self.lockfile)
-                self.fd = os.open(self.lockfile, os.O_CREAT | os.O_EXCL | os.O_RDWR)
+                # file already exists, we try to remove (in case previous
+                # execution was interrupted)
+                if os.path.exists(lockpath):
+                    os.unlink(lockpath)
+                fobj = os.open(lockpath, os.O_CREAT | os.O_EXCL | os.O_RDWR)
             except OSError:
                 type, e, tb = sys.exc_info()
                 if e.errno == 13:
-                    logger.error("Another instance is already running, quitting.")
-                    sys.exit(-1)
+                    return None
                 print(e.errno)
                 raise
         else:  # non Windows
             import fcntl
-            self.fp = open(self.lockfile, 'w')
+            fobj = open(lockpath, 'w')
             try:
-                fcntl.lockf(self.fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+                fcntl.lockf(fobj, fcntl.LOCK_EX | fcntl.LOCK_NB)
             except IOError:
-                logger.warning("Another instance is already running, quitting.")
-                sys.exit(-1)
-        self.initialized = True
+                return None
+
+        return fobj
 
     def __del__(self):
         import sys
@@ -64,13 +98,13 @@ class SingleInstance:
             return
         try:
             if sys.platform == 'win32':
-                if hasattr(self, 'fd'):
-                    os.close(self.fd)
+                if hasattr(self, 'fobj'):
+                    os.close(self.fobj)
                     os.unlink(self.lockfile)
             else:
                 import fcntl
-                fcntl.lockf(self.fp, fcntl.LOCK_UN)
-                # os.close(self.fp)
+                fcntl.lockf(self.fobj, fcntl.LOCK_UN)
+                # os.close(self.fobj)
                 if os.path.isfile(self.lockfile):
                     os.unlink(self.lockfile)
         except Exception as e:
@@ -100,19 +134,27 @@ class testSingleton(unittest.TestCase):
         p = Process(target=f, args=("test-2",))
         p.start()
         p.join()
-        assert p.exitcode == 0, "%s != 0" % p.exitcode  # the called function should succeed
+        # the called function should succeed
+        assert p.exitcode == 0, "%s != 0" % p.exitcode
 
     def test_3(self):
         me = SingleInstance(flavor_id="test-3")
         p = Process(target=f, args=("test-3",))
         p.start()
         p.join()
-        assert p.exitcode != 0, "%s != 0 (2nd execution)" % p.exitcode  # the called function should fail because we already have another instance running
-        # note, we return -1 but this translates to 255 meanwhile we'll consider that anything different from 0 is good
+        # the called function should fail because we already have another
+        # instance running
+        assert p.exitcode != 0, "%s != 0 (2nd execution)" % p.exitcode
+
+        # note, we return -1 but this translates to 255 meanwhile we'll consider
+        # that anything different from 0 is good
         p = Process(target=f, args=("test-3",))
         p.start()
         p.join()
-        assert p.exitcode != 0, "%s != 0 (3rd execution)" % p.exitcode  # the called function should fail because we already have another instance running
+        # the called function should fail because we already have another
+        # instance running
+        assert p.exitcode != 0, "%s != 0 (3rd execution)" % p.exitcode
+
 
 logger = logging.getLogger("tendo.singleton")
 logger.addHandler(logging.StreamHandler())
